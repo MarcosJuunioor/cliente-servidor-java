40b6d46ef457ab38666344f16866a28a5f329907
Marcos Junior <marcosjuunioor2@gmail.com>
2020-03-08 19:44:30 -0300

    Criação dos métodos "codificar e decodificar"

--- Cliente.java
+++ Cliente.java
@@ -30,4 +30,75 @@ public class Cliente
       System.out.println("Conexão encerrada");
       try{System.in.read();}catch(Exception e3){}
   }
+ 
+ 	public static String codificar(String mens){
+	int chave = 3;
+	char ascii;
+	char x, y;
+	String retorno = "";
+
+	for (int i = 0; i < mens.length(); i++) {
+         //Tratamento Letras minusculas  
+            if (mens.charAt(i) >= 97 && mens.charAt(i) <= 122) {//letrans minusculas de acordo com a tabela ASCII
+                if ((int) (mens.charAt(i) + chave) > 122) {
+                    x = (char) (mens.charAt(i) + chave);
+                    y = (char) (x - 122);
+                    ascii = (char) (96 + y);
+                    retorno += ascii + " ";
+                } else {
+                    ascii = (char) (mens.charAt(i) + chave);
+                    retorno += ascii + " ";
+                }
+	}            
+
+	//Tratamento Letras mausculas
+            if (mens.charAt(i) >= 65 && mens.charAt(i) <= 90) {
+                if (mens.charAt(i) + chave > 90) {
+                    x = (char) (mens.charAt(i) + chave);
+                    y = (char) (x - 90);
+                    ascii = (char) (64 + y);
+                    retorno += ascii + " ";
+                } else {
+                    ascii = (char) (mens.charAt(i) + chave);
+                    retorno += ascii + " ";
+                }
+            }
+        }
+
+	return retorno;
+	}
+
+	public static String decodificar(int chave, String mens) {
+            
+            char ascii;
+            char x, y;
+            String retorno = "";
+
+            for (int i = 0; i < mens.length(); i++) {
+             //Tratamento Letras minusculas  
+                if (mens.charAt(i) >= 97 && mens.charAt(i) <= 122) {//letrans minusculas de acordo com a tabela ASCII
+                    if ((int) (mens.charAt(i) - chave) < 97) {
+                    	x = (char) (mens.charAt(i) - chave);
+                    	ascii = (char)(x + 26);
+                        retorno += ascii + " ";
+                    } else {
+                        ascii = (char) (mens.charAt(i) - chave);
+                        retorno += ascii + " ";
+                    }
+	         }            
+
+            //Tratamento Letras mausculas
+                if (mens.charAt(i) >= 65 && mens.charAt(i) <= 90) {
+                    if (mens.charAt(i) - chave < 65) {
+                        x = (char) (mens.charAt(i) - chave);
+                        ascii = (char)(x+26);
+                        retorno += ascii + " ";
+                    } else {
+                        ascii = (char) (mens.charAt(i) - chave);
+                        retorno += ascii + " ";
+                    }
+            }            
+	      }
+            return retorno;
+        }
 }
