6b64e16a8e8870e915eebafda3c5837b92985b83
marcosjuunioor <marcosjuunioor2@gmail.com>
2020-03-09 09:52:48 -0300

    atualização de Cliente e Servidor para trabalharem com palavras

--- Cliente.java
+++ Cliente.java
@@ -1,81 +1,89 @@
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
-public class Cliente
-{ public static void main(String[] arg)
-  { double numero;
-    int c, total = (int)(10*Math.random());
-    Socket s = null;
-    try
-    { System.out.println("Conectando...");
-      s = new Socket("192.168.89.149",6789);
-      DataInputStream in = new DataInputStream(s.getInputStream());
-      DataOutputStream out = new DataOutputStream(s.getOutputStream());
-      System.out.println("Conectado. Enviando "+total+" números...");
-      for(c=0; c<total; c++){
-//       numero = 100.0*Math.random();
-        Scanner sc1 = new Scanner(System.in); 
-        numero =  sc1.nextDouble();
-        System.out.println("Enviando "+numero);
-        out.writeDouble(numero*3.14);
-      }
-      out.flush();
-      out.writeDouble(-1.0);
-      System.out.println("Somatório = "+(in.readDouble()/3.14));
-      }
-      catch(Exception e)
-      { System.out.println("Erro: "+e.getMessage()); }
-      finally
-      { try{if(s!=null) s.close();}catch(Exception e2){}    }
-      System.out.println("Conexão encerrada");
-      try{System.in.read();}catch(Exception e3){}
-  }
+public class Cliente{ 
+	public static void main(String[] arg){ 
+		String palavra;
+	    int c, total = (int)(10*Math.random());
+	    Socket s = null;
+	    
+	    try{ 
+	    	System.out.println("Conectando...");
+	    	s = new Socket("192.168.89.149",6789);
+	    	DataInputStream in = new DataInputStream(s.getInputStream());
+			DataOutputStream out = new DataOutputStream(s.getOutputStream());
+			System.out.println("Conectado. Enviando "+total+" números...");
+			
+			for(c=0; c<total; c++){
+				//       numero = 100.0*Math.random();
+				Scanner sc1 = new Scanner(System.in); 
+				palavra =  sc1.next();
+				System.out.println("Enviando "+palavra);
+				palavra = codificar(palavra);
+				out.writeUTF(palavra);
+			}
+			out.flush();
+			out.writeUTF("fim");
+			System.out.println("Frase = "+in.readUTF());
+	    }
+	    catch(Exception e){ 
+	    	System.out.println("Erro: "+e.getMessage()); 
+	    }
+	    finally{ 
+	    	try{
+	    		if(s!=null) s.close();
+	    	}catch(Exception e2){}    
+    	}
+	    System.out.println("Conexão encerrada");
+	    try{
+	    	System.in.read();
+    	}catch(Exception e3){}
+	}
  
- 	public static String codificar(String mens){
-	int chave = 3;
-	char ascii;
-	char x, y;
-	String retorno = "";
-
-	for (int i = 0; i < mens.length(); i++) {
-         //Tratamento Letras minusculas  
-            if (mens.charAt(i) >= 97 && mens.charAt(i) <= 122) {//letrans minusculas de acordo com a tabela ASCII
-                if ((int) (mens.charAt(i) + chave) > 122) {
-                    x = (char) (mens.charAt(i) + chave);
-                    y = (char) (x - 122);
-                    ascii = (char) (96 + y);
-                    retorno += ascii + " ";
-                } else {
-                    ascii = (char) (mens.charAt(i) + chave);
-                    retorno += ascii + " ";
-                }
-	}            
-
-	//Tratamento Letras mausculas
-            if (mens.charAt(i) >= 65 && mens.charAt(i) <= 90) {
-                if (mens.charAt(i) + chave > 90) {
-                    x = (char) (mens.charAt(i) + chave);
-                    y = (char) (x - 90);
-                    ascii = (char) (64 + y);
-                    retorno += ascii + " ";
-                } else {
-                    ascii = (char) (mens.charAt(i) + chave);
-                    retorno += ascii + " ";
-                }
-            }
-        }
-
-	return retorno;
+	public static String codificar(String mens){
+		int chave = 3;
+		char ascii;
+		char x, y;
+		String retorno = "";
+	
+		for (int i = 0; i < mens.length(); i++) {
+	         	//Tratamento Letras minusculas  
+	            if (mens.charAt(i) >= 97 && mens.charAt(i) <= 122) {//letrans minusculas de acordo com a tabela ASCII
+	                if ((int) (mens.charAt(i) + chave) > 122) {
+	                    x = (char) (mens.charAt(i) + chave);
+	                    y = (char) (x - 122);
+	                    ascii = (char) (96 + y);
+	                    retorno += ascii + " ";
+	                } else {
+	                    ascii = (char) (mens.charAt(i) + chave);
+	                    retorno += ascii + " ";
+	                }
+	            }            
+	
+	            //Tratamento Letras maiusculas
+	            if (mens.charAt(i) >= 65 && mens.charAt(i) <= 90) {
+	                if (mens.charAt(i) + chave > 90) {
+	                    x = (char) (mens.charAt(i) + chave);
+	                    y = (char) (x - 90);
+	                    ascii = (char) (64 + y);
+	                    retorno += ascii + " ";
+	                } else {
+	                    ascii = (char) (mens.charAt(i) + chave);
+	                    retorno += ascii + " ";
+	                }
+	            }
+		}
+	
+		return retorno;
 	}
 
 	public static String decodificar(int chave, String mens) {
-            
             char ascii;
             char x, y;
             String retorno = "";
 
             for (int i = 0; i < mens.length(); i++) {
-             //Tratamento Letras minusculas  
+            	//Tratamento Letras minusculas  
                 if (mens.charAt(i) >= 97 && mens.charAt(i) <= 122) {//letrans minusculas de acordo com a tabela ASCII
                     if ((int) (mens.charAt(i) - chave) < 97) {
                     	x = (char) (mens.charAt(i) - chave);
@@ -85,9 +93,9 @@ public class Cliente
                         ascii = (char) (mens.charAt(i) - chave);
                         retorno += ascii + " ";
                     }
-	         }            
+                }            
 
-            //Tratamento Letras mausculas
+                //Tratamento Letras mausculas
                 if (mens.charAt(i) >= 65 && mens.charAt(i) <= 90) {
                     if (mens.charAt(i) - chave < 65) {
                         x = (char) (mens.charAt(i) - chave);
@@ -97,8 +105,8 @@ public class Cliente
                         ascii = (char) (mens.charAt(i) - chave);
                         retorno += ascii + " ";
                     }
-            }            
+                }            
 	      }
             return retorno;
-        }
+	}
 }
--- Servidor.java
+++ Servidor.java
@@ -1,38 +1,106 @@
 import java.io.*; 
 import java.net.*; 
 import java.util.*; 
-public class Servidor 
-{ 
-public static void main(String[] args ) 
-{ ServerSocket s;
-    try
-    { s = new ServerSocket(6789);
-      System.out.println("Servidor iniciado na porta 6789");
-      while(true)
-      { Socket cliente = s.accept();
-        System.out.println("Conexão estabelecida "+
-                           "("+cliente+")");
-        DataInputStream in = 
-              new DataInputStream(cliente.getInputStream());
-        DataOutputStream out = 
-            new DataOutputStream(cliente.getOutputStream());
-        double numero, soma = 0.0;
-        numero = in.readDouble();
-        while(numero != -1.0)
-        { 
-          numero = numero/3.14;
-          soma += numero;
-          System.out.println("Valor recebido: "+numero+
-                             " \t - parcial = "+soma);
-          numero = in.readDouble();
-        }
-        out.writeDouble(soma * 3.14);
-        cliente.close();
-        System.out.println("Conexão encerrada.");
-      }
-    }
-    catch(Exception e)
-    { System.out.println("Erro: "+e.getMessage());
-    }
-  }
+public class Servidor{ 
+	public static void main(String[] args ) { 
+		ServerSocket s;
+	    try{ 
+	    	s = new ServerSocket(6789);
+	    	System.out.println("Servidor iniciado na porta 6789");
+	      
+	    	while(true){ 
+	    		Socket cliente = s.accept();
+	    		System.out.println("Conexão estabelecida "+
+			                   "("+cliente+")");
+	    		DataInputStream in = new DataInputStream(cliente.getInputStream());
+	    		DataOutputStream out = new DataOutputStream(cliente.getOutputStream());
+	    		String palavra, frase = "";
+	    		palavra = in.readUTF();
+			  
+	    		while(!palavra.equals("fim")){ 
+	    			
+	    			frase += palavra;
+	    			System.out.println("Palavra recebida: "+palavra+
+			                     " \t - parcial = "+frase);
+	    			palavra = in.readUTF();
+	    		}
+	    		out.writeUTF(frase);
+	    		cliente.close();
+	    		System.out.println("Conexão encerrada.");
+	    	}
+	    }
+	    catch(Exception e){ 
+	    	System.out.println("Erro: "+e.getMessage());
+	    }
+	}
+	
+	public static String codificar(String mens){
+		int chave = 3;
+		char ascii;
+		char x, y;
+		String retorno = "";
+	
+		for (int i = 0; i < mens.length(); i++) {
+	         	//Tratamento Letras minusculas  
+	            if (mens.charAt(i) >= 97 && mens.charAt(i) <= 122) {//letrans minusculas de acordo com a tabela ASCII
+	                if ((int) (mens.charAt(i) + chave) > 122) {
+	                    x = (char) (mens.charAt(i) + chave);
+	                    y = (char) (x - 122);
+	                    ascii = (char) (96 + y);
+	                    retorno += ascii + " ";
+	                } else {
+	                    ascii = (char) (mens.charAt(i) + chave);
+	                    retorno += ascii + " ";
+	                }
+	            }            
+	
+	            //Tratamento Letras maiusculas
+	            if (mens.charAt(i) >= 65 && mens.charAt(i) <= 90) {
+	                if (mens.charAt(i) + chave > 90) {
+	                    x = (char) (mens.charAt(i) + chave);
+	                    y = (char) (x - 90);
+	                    ascii = (char) (64 + y);
+	                    retorno += ascii + " ";
+	                } else {
+	                    ascii = (char) (mens.charAt(i) + chave);
+	                    retorno += ascii + " ";
+	                }
+	            }
+		}
+	
+		return retorno;
+	}
+
+	public static String decodificar(int chave, String mens) {
+            char ascii;
+            char x, y;
+            String retorno = "";
+
+            for (int i = 0; i < mens.length(); i++) {
+            	//Tratamento Letras minusculas  
+                if (mens.charAt(i) >= 97 && mens.charAt(i) <= 122) {//letrans minusculas de acordo com a tabela ASCII
+                    if ((int) (mens.charAt(i) - chave) < 97) {
+                    	x = (char) (mens.charAt(i) - chave);
+                    	ascii = (char)(x + 26);
+                        retorno += ascii + " ";
+                    } else {
+                        ascii = (char) (mens.charAt(i) - chave);
+                        retorno += ascii + " ";
+                    }
+                }            
+
+                //Tratamento Letras mausculas
+                if (mens.charAt(i) >= 65 && mens.charAt(i) <= 90) {
+                    if (mens.charAt(i) - chave < 65) {
+                        x = (char) (mens.charAt(i) - chave);
+                        ascii = (char)(x+26);
+                        retorno += ascii + " ";
+                    } else {
+                        ascii = (char) (mens.charAt(i) - chave);
+                        retorno += ascii + " ";
+                    }
+                }            
+	      }
+            return retorno;
+	}
 }
